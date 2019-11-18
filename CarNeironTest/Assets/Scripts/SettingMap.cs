7db0408b251fe1a4985065e07032cf61f3cf098c
DOG007ua <dogua007@gmail.com>
2019-11-18 17:16:37 +0200

    All project

---
+++ CarNeironTest/Assets/Scripts/SettingMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingMap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hand = GameObject.Find("Monster/Arm/Hand");
+        //obj.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
